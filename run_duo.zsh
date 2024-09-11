cc045827089805f91bd045d31ab24b0c596f1a0d
NataliaKowalczyk01 <162929979+NataliaKowalczyk01@users.noreply.github.com>
2024-09-11 20:36:16 +0200

    Update run_duo.zsh
    New folder name

--- run_duo.zsh
+++ run_duo.zsh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 python3 src/train_amplify_duo.py \
-    -amp_act_tr ./data_test/train_activity_AMP.fa \
-    -non_amp_act_tr ./data_test/train_activity_nonAMP.fa \
-    -amp_tox_tr ./data_test/train_toxicity_AMP.fa \
-    -non_amp_tox_tr ./data_test/train_toxicity_nonAMP.fa \
+    -amp_act_tr ./data_union_ecoli_saureus/union_active_Ecoli_tr.fa \
+    -non_amp_act_tr ./data_union_ecoli_saureus/union_inactive_Ecoli_tr.fa \
+    -amp_tox_tr ./data_union_ecoli_saureus/union_active_Step_tr.fa \
+    -non_amp_tox_tr ./data_union_ecoli_saureus/union_inactive_Step_tr.fa \
     -out_dir ./experiment_3 \
     -model_name amplify_duo
